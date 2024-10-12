27dacb001a316deeee13e66c7f651a9ab4088394
miraybatki <miraybatki01@gmail.com>
2024-10-13 02:02:09 +0300

    don't forget

--- components/stocks/message.tsx
+++ components/stocks/message.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { IconOpenAI, IconUser } from '@/components/ui/icons'
+import { IconUser } from '@/components/ui/icons'
+import Image from 'next/image'
 import { cn } from '@/lib/utils'
 import { spinner } from './spinner'
 import { CodeBlock } from '../ui/codeblock'
@@ -36,8 +37,8 @@ export function BotMessage({
 
   return (
     <div className={cn('group relative flex items-start md:-ml-12', className)}>
-      <div className="flex size-[24px] shrink-0 select-none items-center justify-center rounded-md border bg-primary text-primary-foreground shadow-sm">
-        <IconOpenAI />
+      <div className="flex size-[24px] shrink-0 select-none items-center justify-center rounded-md overflow-hidden border bg-primary text-primary-foreground shadow-sm">
+        <Image src="/wildy.png" alt="Bot Avatar" width={24} height={24} />
       </div>
       <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
         <MemoizedReactMarkdown
@@ -97,11 +98,11 @@ export function BotCard({
     <div className="group relative flex items-start md:-ml-12">
       <div
         className={cn(
-          'flex size-[24px] shrink-0 select-none items-center justify-center rounded-md border bg-primary text-primary-foreground shadow-sm',
+          'flex size-[24px] shrink-0 select-none items-center justify-center rounded-md overflow-hidden border bg-primary text-primary-foreground shadow-sm',
           !showAvatar && 'invisible'
         )}
       >
-        <IconOpenAI />
+        <Image src="/wildy.png" alt="Bot Avatar" width={24} height={24} />
       </div>
       <div className="ml-4 flex-1 pl-2">{children}</div>
     </div>
@@ -123,8 +124,8 @@ export function SystemMessage({ children }: { children: React.ReactNode }) {
 export function SpinnerMessage() {
   return (
     <div className="group relative flex items-start md:-ml-12">
-      <div className="flex size-[24px] shrink-0 select-none items-center justify-center rounded-md border bg-primary text-primary-foreground shadow-sm">
-        <IconOpenAI />
+      <div className="flex size-[24px] shrink-0 select-none items-center justify-center rounded-md overflow-hidden border bg-primary text-primary-foreground shadow-sm">
+        <Image src="/wildy.png" alt="Bot Avatar" width={24} height={24} />
       </div>
       <div className="ml-4 h-[24px] flex flex-row items-center flex-1 space-y-2 overflow-hidden px-1">
         {spinner}
